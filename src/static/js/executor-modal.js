3eeab225224b08e124b5085eed8cc91489e12bc8
Mariana <mariana.izumiks@gmail.com>
2022-05-03 18:18:55 -0300

    Adicionando teste de modal e botão de recusa

---
+++ src/static/js/executor-modal.js
@@ -0,0 +1,11 @@
+function modal(){
+   let modal = document.querySelector('.modal')
+
+   modal.style.display = 'block';
+}
+
+function fechar(){
+   let modal = document.querySelector('.modal')
+
+   modal.style.display = 'none';
+}
