e1d5c6443fb27445d7a84f1b9124b2b77b425310
Mariana <mariana.izumiks@gmail.com>
2022-06-01 10:17:58 -0300

    Alterando evento do card de relatorios na home adimin

---
+++ src/static/js/opcoesRelatorios.js
@@ -0,0 +1,5 @@
+function abreOpcoes() {
+    document.getElementById('opcoesRel').style.width = '250px';
+    document.getElementById('opcoesRel').style.display = 'flex';
+    document.getElementById('opcoesRel').style.background = '#fff';
+}
